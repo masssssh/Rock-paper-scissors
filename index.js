1dabbcd41a0f575a6a26e4334c7f0898b3131f2a
masssssh <pykpykpyk000@yandex.ru>
2024-11-19 02:27:40 +0300

    add DOM

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-function getComputerChoise () {
+let roundResultSec = document.querySelector('#roundResult');
+let score = document.querySelector('#score');
 
+function getComputerChoice () {
     let ComputerChoice = Math.random ( );
-
-    if (ComputerChoice < 0.33) 
-        return "камень";
-
-    else if (ComputerChoice < 0.66)
-        return "ножницы";
-
-    else 
-        return "бумага";
-
+        if (ComputerChoice < 0.33) 
+            return "камень";
+        else if (ComputerChoice < 0.66)
+            return "ножницы";   
+        else 
+            return "бумага";
 }
 
 function getHumanChoice () {
@@ -20,56 +18,59 @@ function getHumanChoice () {
 
 let HumanScore = 0;
 let ComputerScore = 0;
+let roundResult = "";
 
 function PlayRound (ComputerChoice, HumanChoice) {
 
     if (ComputerChoice === "камень" && HumanChoice === "бумага") {
-        console.log("Поздравляю! Бумага бьет камень!");
+        roundResult = "Поздравляю! Бумага бьет камень!";
         HumanScore++;
     }
-
     else if (ComputerChoice === "камень" && HumanChoice === "ножницы") {
-        console.log("Увы! Камень бьет бумагу.");
+        roundResult = "Увы! Камень бьет бумагу.";
         ComputerScore++;
     }
-
     else if (ComputerChoice === "ножницы" && HumanChoice === "бумага") {
-        console.log("Увы! Ножницы бьют бумагу.");
+        roundResult = "Увы! Ножницы бьют бумагу.";
         ComputerScore++;
     }
-
     else if (ComputerChoice === "ножницы" && HumanChoice === "камень") {
-        console.log("Поздравляю! Камень бьет ножницы!");
+        roundResult = "Поздравляю! Камень бьет ножницы!";
         HumanScore++;
     } 
-
     else if (ComputerChoice === "бумага" && HumanChoice === "камень") {
-        console.log("Увы! Бумага бьют камень.");
+        roundResult = "Увы! Бумага бьют камень.";
         ComputerScore++;
     }
-
     else if (ComputerChoice === "бумага" && HumanChoice === "ножницы") {
-        console.log("Поздравляю! Ножницы бьют бумагу!");
+        roundResult = "Поздравляю! Ножницы бьют бумагу!";
         HumanScore++;
     }
-
     else { 
-        console.log("Ничья.");
+        roundResult = "Ничья.";
     }
+
+roundResultSec.textContent = `Результаты раунда: ${roundResult}`;
+score.textContent = `Счет: вы - ${HumanScore} очков, компьтер - ${ComputerScore}`;
+
+if (HumanScore === 5 || ComputerScore === 5) {
+    const winner = HumanScore === 5 ? "Вы победили!" : "Компьютер победил!";
+        alert(`${winner} Финальный счет: вы - ${HumanScore}, компьютер - ${ComputerScore}`);
+}
 }
 
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
 
+rockBtn.addEventListener ('click', () => {
+    PlayRound (getComputerChoice (), 'камень');
+});
 
+paperBtn.addEventListener ('click', () => {
+    PlayRound (getComputerChoice (), 'бумага');
+});
 
-for (let i = 0; i < 5; i++) {
-    
-    console.log(`Раунд ${i + 1}`);
-    let HumanChoice = getHumanChoice().toLowerCase();
-    let СomputerChoice = getComputerChoise();
-    PlayRound(СomputerChoice, HumanChoice);
-    console.log(ComputerScore);
-    console.log(HumanScore);
-}
-
-
-console.log(("Результаты: Вы набрали " + HumanScore + " очков, компьютер - " + ComputerScore ));
+scissorsBtn.addEventListener ('click', () => {
+    PlayRound (getComputerChoice (), 'ножницы');
+});
